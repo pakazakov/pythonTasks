67d107d1d71cc1e2ccb248c4da746e9fa6a101ec
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-10-05 19:48:25 +0300

    gg

--- 6.4.py
+++ 6.4.py
@@ -5,7 +5,7 @@ for word in words.split():
     word = ' '.join(word)
     if word.istitle()==True:
         sumTitle += 1
-    if word[-1] in '.,:;!?':
+    if word.rfind('.',',','!','?',':',';'):
         sumSigns += 1
 
 print(f'Количество слов начинающихся с заглавной буквы - {sumTitle}\n Количество слов заканчивающихся знаком препинания - {sumSigns}')
