f31b85b2ffd81715854eb43bf602d90052e11b82
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-10-05 19:26:14 +0300

    111

---
+++ 6.4.py
@@ -0,0 +1,11 @@
+words = input('Введите строку\n')
+sumTitle = 0
+sumSigns = 0
+for word in words.split():
+    word = ' '.join(word)
+    if word.istitle()==True:
+        sumTitle += 1
+    if word[-1] in '.,:;!?':
+        sumSigns += 1
+
+print(f'Количество слов начинающихся с заглавной буквы - {sumTitle}\n Количество слов заканчивающихся знаком препинания - {sumSigns}')
