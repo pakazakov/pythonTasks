73c81727af6246c62761a21505663c1819fc3e5c
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-10-09 17:38:50 +0300

    ready

--- 6.1.py
+++ 6.1.py
@@ -1,8 +1,8 @@
-import numpy as np
+from numpy import *
 
 list1 = list(map(int,input().split()))
-vector1 = np.array(list1)
+vector1 = array(list1)
 list2 = list(map(int,input().split()))
-vector2 = np.array(list2)
+vector2 = array(list2)
 
 print(vector1*vector2)
