36d6adf0d7458429307575fae18d37bc7a32fab5
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-10-09 17:45:19 +0300

    ready

--- 6.1.py
+++ 6.1.py
@@ -1,8 +1,10 @@
 from numpy import *
 
-list1 = list(map(int,input().split()))
+'''list1 = list(map(int,input().split('Введите первый массив: ')))
 vector1 = array(list1)
-list2 = list(map(int,input().split()))
+list2 = list(map(int,input().split('Введите второй массив: ')))
 vector2 = array(list2)
 
-print(vector1*vector2)
+print(vector1*vector2)'''
+
+print(array(list(map(int,input('Введите первый массив: ').split())))*array(list(map(int,input('Введите второй массив: ').split()))))
