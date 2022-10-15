73c1d67716e250d196b848d716f6472dbe7793e2
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-10-15 18:45:06 +0300

    ready

---
+++ 8.3.py
@@ -0,0 +1,19 @@
+dic = {'привет': 'hello',
+       'мир': 'world',
+       'ты': 'you',
+       'я': 'i',
+       'сделал': 'made',
+       'словарь': 'dictionary'}
+
+text = input('Введите текст: ').strip().split()
+
+arr = []
+try:
+       for i in range(len(text)):
+              word = ''.join(text[i])
+              for key, val in dic.items():
+                     arr.append(dic[word]+' ')
+                     break
+       print(''.join(arr))
+except:
+       print('К сожалению мы пока не можем перевести этот текст')
