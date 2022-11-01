a5f72fe109114c256d4d98d09e8c7babe10acc47
podpivasik <114077096+podpivasik@users.noreply.github.com>
2022-11-01 11:27:39 +0300

    ready

--- 9/9.2.py
+++ 9/9.2.py
@@ -3,7 +3,7 @@ import re
 with open("2.txt", "r") as f:
     text = re.split(r"[.?!]+", f.read())
 
-n = int(input())
+n = int(input("Введите заданное количество слов: "))
 
 text = map(lambda x: list(x.strip().split()), text)
 text = filter(lambda x: len(x) > n, text)
@@ -11,3 +11,9 @@ text = map(lambda x: " ".join(x), text)
 
 with open("2_result.txt", "w") as f:
     print("\n".join(text), file=f)
+
+""" Дан файл с текстом из нескольких предложений. Предложение может
+заканчиваться одним из трёх символов '.', '?', '!'. Предложение — это
+набор слов, разделённых пробелами. Выведите в новый файл все
+предложения, в которых более заданного количества слов.
+"""
